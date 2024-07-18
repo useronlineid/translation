f6a9e4e25b23efcae80fa4924c3acf51ffbd46a0
useronlineid <dmcode1010@gmail.com>
2024-07-18 18:09:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,180 +1,149 @@
-document.getElementById('saveImage').addEventListener('click', function() {
-    const HouseCodeNo = document.getElementById('HouseCodeNo').value || "-";
-    const RegistrationOffice = document.getElementById('RegistrationOffice').value || "-";
-    const Addresslist = document.getElementById('Addresslist').value || "-";
-    const NameofHouse = document.getElementById('NameofHouse').value || "-";
-    const NameofVillage = document.getElementById('NameofVillage').value || "-";
-    const TypeofHouse = document.getElementById('TypeofHouse').value || "-";
-    const DescriptionofHouse = document.getElementById('DescriptionofHouse').value || "-";
-    const Housenumber = document.getElementById('Housenumber').value || "-";
-    const Signed = document.getElementById('Signed').value || "-";
-    const RegistrationPrinting = document.getElementById('RegistrationPrinting').value || "-";
-
-
-    const BookNo = document.getElementById('BookNo').value || "-";
-    const HouseCodeNo1 = document.getElementById('HouseCodeNo1').value || "-";
-    const Name = document.getElementById('Name').value || "-";
-    const Nationality = document.getElementById('Nationality').value || "-";
-    const Sex = document.getElementById('Sex').value || "-";
-    const Identification = document.getElementById('Identification').value || "-";
-    const Status = document.getElementById('Status').value || "-";
-    const Dateofbirth = document.getElementById('Dateofbirth').value || "-";
-    const Mother = document.getElementById('Mother').value || "-";
-    const Identification1 = document.getElementById('Identification1').value || "-";
-    const Nationality1 = document.getElementById('Nationality1').value || "-";
-    const Father = document.getElementById('Father').value || "-";
-    const Identification2 = document.getElementById('Identification2').value || "-";
-    const Nationality2 = document.getElementById('Nationality2').value || "-";
-    const TransferredFrom = document.getElementById('TransferredFrom').value || "-";
-    const Signed1 = document.getElementById('Signed1').value || "-";
-    const certifiedcorrecttranslation = document.getElementById('certifiedcorrecttranslation').value || "-";
+function updateDisplay() {
+    const houseCode = document.getElementById('houseCode').value || '-';
+    const RegistrationOffice = document.getElementById('RegistrationOffice').value || '-';
+    const addressList = document.getElementById('addressList').value || '-';
+    const nameOfVillage = document.getElementById('nameOfVillage').value || '-';
+    const nameOfHouse = document.getElementById('nameOfHouse').value || '-';
+    const typeOfHouse = document.getElementById('typeOfHouse').value || '-';
+    const descriptionOfHouse = document.getElementById('descriptionOfHouse').value || '-';
+    const houseNumberDate = document.getElementById('houseNumberDate').value || '-';
+    const signed = document.getElementById('signed').value || '-';
+    const registrationDate = document.getElementById('registrationDate').value || '-';
     
-    // สร้างหน้าใหม่และเปิด
-    const outputWindow = window.open('', '', 'width=674.5,height=953.5');
-    outputWindow.document.write(`
-        <html>
-        <head>
-            <link rel="stylesheet" href="styles-output.css">
-        </head>
-        <body>
-            <div id="capture">
-                <img src="https://github.com/useronlineid/translation/blob/main/CM.jpg?raw=true" alt="Image">
-                <p style="position: absolute; top: 85px; left: 275px; color:#252525; font-size: 12px; font-weight: bold;">
-                House Particulars
-                </p>     
-                
-                <p style="position: absolute; top: 85px; left: 500px; color:#252525; font-size: 12px; font-weight: bold;">
-                Book No. 1
-                </p>     
-                <p style="position: absolute; top: 120px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;">
-                House Code No.: ${HouseCodeNo}
-                </p>   
-                <p style="position: absolute; top: 120px; left: 260px; color:#252525; font-size: 12px; font-weight: bold;">
-                Registration Office: ${RegistrationOffice}
-                </p>   
-                
-                <p style="position: absolute; top: 160px; left: 70px; color:#252525; font-size: 12px; font-weight: bold;">
-                Address list: <br>${Addresslist}
-                </p>   
-                
-                <p style="position: absolute; top: 220px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;">
-                Name of Village: ${NameofVillage}
-                </p>   
-                <p style="position: absolute; top: 255px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;">
-                Type of House: ${TypeofHouse}
-                </p>   
-                <p style="position: absolute; top: 220px; left: 320px; color:#252525; font-size: 12px; font-weight: bold;">
-                Name of House: ${NameofHouse}
-                </p>   
-                <p style="position: absolute; top: 255px; left: 320px; color:#252525; font-size: 12px; font-weight: bold;">
-                Description of House: ${DescriptionofHouse}
-                </p>   
-                
-                <p style="position: absolute; top: 285px; left: 55px; color:#252525; font-size: 12px; font-weight: bold;">
-                Date of month,year to determine the house number: ${DescriptionofHouse}
-                </p>   
-                
-                <p style="position: absolute; top: 325px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;">
-                Registrar
-                </p>   
-                
-                <p style="position: absolute; top: 350px; left: 80%; color:#252525; font-size: 12px; font-weight: bold; text-align: right; transform: translateX(-100%);width: 95%;"">
-                Signed:
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                ${Signed}
-                </p>   
-                
-                
-                <p style="position: absolute; top: 458px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;">
-                Book No. 1
-                </p>   
-                <p style="position: absolute; top: 458px; left: 140px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Particulars of Persons in the House Code No : ${HouseCodeNo1}
-                </p>   
-                
-                <p style="position: absolute; top: 458px; left: 500px; color:#252525; font-size: 12px; font-weight: bold;">
-                Book No. ${BookNo}
-                </p>     
-                
-                <p style="position: absolute; top: 490px; left: 70px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Name: ${Name}
-                </p>   
-                <p style="position: absolute; top: 530px; left: 70px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Identification No.: ${Identification}
-                </p>   
-                <p style="position: absolute; top: 570px; left: 70px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Name of Biological Mother: ${Mother}
-                </p>  
-                <p style="position: absolute; top: 600px; left: 70px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Name of Biological Father: ${Father}
-                </p>  
-                
-                <p style="position: absolute; top: 490px; left: 320px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Nationality: ${Nationality}
-                </p>   
-                <p style="position: absolute; top: 530px; left: 310px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Status: ${Status}
-                </p>   
-                <p style="position: absolute; top: 570px; left: 300px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                ${Identification1}
-                </p>  
-                <p style="position: absolute; top: 600px; left: 300px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                ${Identification2}
-                </p>  
-                
-                <p style="position: absolute; top: 490px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Sex: ${Sex}
-                </p>   
-                <p style="position: absolute; top: 530px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Date of Birth: ${Dateofbirth}
-                </p>   
-                <p style="position: absolute; top: 570px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Nationality:${Nationality1}
-                </p>  
-                <p style="position: absolute; top: 600px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                Nationality:${Nationality2}
-                </p>  
-                
-                
-                <p style="position: absolute; top: 630px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                * Transferred from: ${TransferredFrom}
-                </p>  
-                
-                <p style="position: absolute; top: 655px; left: 450px; color:#252525; font-size: 12px; font-weight: bold;">
-                Registrar
-                </p>   
-                
-                <p style="position: absolute; top: 680px; left: 80%; color:#252525; font-size: 12px; font-weight: bold;  text-align: right; transform: translateX(-100%);width: 95%;">
-                Signed:
-                &nbsp;&nbsp;&nbsp;&nbsp;
-                ${Signed1}
-                </p> 
-                
-                <p style="position: absolute; top: 730px; left: 60px; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px;">
-                ** Transferred to
-                </p>  
-                
-                <p style="position: absolute; top: 800px; left: 70%; color:#252525; font-size: 12px; font-weight: bold;letter-spacing: -0.3px; text-align: center; transform: translateX(-50%);">
-                Certified Correct Translation
-                <br>
-                <br>
-                (${certifiedcorrecttranslation})
-                </p>   
-                
-             </div>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
-            <script>
-                window.onload = function() {
-                    html2canvas(document.getElementById('capture')).then(canvas => {
-                        document.body.appendChild(canvas);
-                        const link = document.createElement('a');
-                        link.download = 'output.png';
-                        link.href = canvas.toDataURL();
-                        link.click();
-                    });
-                };
-            </script>
-        </body>
-        </html>
-    `);
-});
+    const bookNo = document.getElementById('bookNo').value || '-';
+    const houseCodeBack = document.getElementById('houseCodeBack').value || '-';
+    const name1 = document.getElementById('name1').value || '-';
+    const nationality = document.getElementById('nationality').value || '-';
+    const sex = document.getElementById('sex').value || '-';
+    const identification = document.getElementById('identification').value || '-';
+    const status = document.getElementById('status').value || '-';
+    const dob = document.getElementById('dob').value || '-';
+    const mother = document.getElementById('mother').value || '-';
+    const identification1 = document.getElementById('identification1').value || '-';
+    const nationality1 = document.getElementById('nationality1').value || '-';
+    const father = document.getElementById('father').value || '-';
+    const identification2 = document.getElementById('identification2').value || '-';
+    const nationality2 = document.getElementById('nationality2').value || '-';
+    const transferredFrom = document.getElementById('transferredFrom').value || '-';
+    const signed1 = document.getElementById('signed1').value || '-';
+    const certified = document.getElementById('certified').value || '-';
+    
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    
+    // Load background image
+    const backgroundImage = new Image();
+    backgroundImage.src = 'https://github.com/useronlineid/translation/blob/main/CM.jpg?raw=true';
+    backgroundImage.onload = function() {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // Draw background image
+        ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+        
+        // Draw text with custom styles, line heights, and letter spacing
+        drawText(ctx, `House Particulars`, 0, 130, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, -17, 0,300);
+        drawText(ctx, `Book No. 1`, 578, 130, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0);
+        
+        drawText(ctx, `House Code No.: ${houseCode}`, 74.4, 168, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0, 250);
+        drawText(ctx, `Registration Office: ${RegistrationOffice}`, 332.6, 168, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,400);
+        drawText(ctx, `Address list: ${addressList}`, 83.5, 208.9, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,400);
+        drawText(ctx, `Name of Village: ${nameOfVillage}`, 74.4, 286.4, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,335);
+        drawText(ctx, `Name of House: ${nameOfHouse}`, 398, 286.4, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,335);
+        drawText(ctx, `Type of House: ${typeOfHouse}`, 74.4, 326.9, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,335);
+        drawText(ctx, `Description of House: ${descriptionOfHouse}`, 398, 326.9, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,335);
+        drawText(ctx, `Date of month,year to determine the house number: ${registrationDate}`, 67.8, 468.1, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,680);
+
+        drawText(ctx, `Registrar`, 0, 405.4, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,285);
+        drawText(ctx, `Signed:     ${signed}`, 0, 430.4, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,285);
+
+        drawText(ctx, `Date of House Registration Printing: ${houseNumberDate}`, 74.4, 356.7, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0,680);
+       
+        drawText(ctx, `Book No. 1`, 74.4, 561, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0, 250);
+        drawText(ctx, `Particulars of Persons in the House Code No: ${houseCodeBack}`, 165.5, 561, '13px Arial', '#000000', '700', 'left', 1.5, -0.25, 0, 0, 400);
+        drawText(ctx, `Book No. ${bookNo}`, 578, 561, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,150);
+        drawText(ctx, `Name: ${name1}`, 74.4, 592, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,270);
+        drawText(ctx, `Nationality: ${nationality}`, 362.4, 592, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,150);
+        drawText(ctx, `Sex: ${sex}`, 523.0, 592, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,210);
+        drawText(ctx, `Identification No.: ${identification}`, 74.4, 636.9, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,270);
+        drawText(ctx, `Status: ${status}`, 346.6, 636.9, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,270);
+        drawText(ctx, `Date of Birth: ${dob}`, 523.0, 636.9, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,210);
+
+        drawText(ctx, `Name of Biological Mother: ${mother}`, 74.4, 680.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,270);
+        drawText(ctx, `${identification1}`, 340.8, 680.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,170);
+        drawText(ctx, `Nationality: ${nationality1}`, 523.0, 680.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,210);
+        
+        drawText(ctx, `Name of Biological Mother: ${father}`, 74.4, 715.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,270);
+        drawText(ctx, `${identification2}`, 340.8, 715.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,170);
+        drawText(ctx, `Nationality: ${nationality2}`, 523.0, 715.0, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,210);
+        
+        drawText(ctx, `* Transferred from:`, 74.4, 758.6, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,500);
+        drawText(ctx, `${transferredFrom}`, 194.2, 758.6, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,500);
+
+        drawText(ctx, `Registrar`, 0, 814.5, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,285);
+        drawText(ctx, `Signed:     ${signed1}`, 0, 839.5, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,285);
+
+        drawText(ctx, `** Transferred to`, 66.9, 888.4, '13px Arial', '#000000', '700', 'left', 1.4, -0.25, 0, 0,500);
+
+        drawText(ctx, `Certified Correct Translation`, 0, 985.2, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,285);
+        drawText(ctx, `(${certified})`, 0, 1021.2, '13px Arial', '#000000', '700', 'center', 1.5, -0.25, 201, 0,330);
+
+        // Continue drawing other fields similarly with the desired line heights and letter spacing
+    };
+}
+
+function drawText(ctx, text, x, y, font, color, weight, align = 'center', lineHeight = 1.9, letterSpacing = 1, adjustedX = 0, adjustedY = 0, maxWidth = 500) {
+    ctx.font = `${weight} ${font}`;
+    ctx.fillStyle = color;
+    
+    const paragraphs = text.split('<br>');
+    let currentY = y;
+
+    paragraphs.forEach(paragraph => {
+        const lines = [];
+        let currentLine = '';
+        
+        for (let i = 0; i < paragraph.length; i++) {
+            currentLine += paragraph[i];
+            const metrics = ctx.measureText(currentLine);
+            
+            if (metrics.width > maxWidth) {
+                lines.push(currentLine.slice(0, -1));
+                currentLine = paragraph[i];
+            }
+        }
+        lines.push(currentLine);
+        
+        lines.forEach((line, index) => {
+            let currentX = x;
+            const textWidth = ctx.measureText(line).width;
+            
+            if (align === 'center') {
+                currentX = (ctx.canvas.width - textWidth) / 2 + adjustedX;
+            } else if (align === 'left') {
+                currentX = x + adjustedX;
+            } else if (align === 'right') {
+                currentX = ctx.canvas.width - textWidth + adjustedX;
+            }
+
+            currentY = y + (index * parseInt(font, 10) * lineHeight);
+
+            for (let j = 0; j < line.length; j++) {
+                ctx.fillText(line[j], currentX, currentY);
+                currentX += ctx.measureText(line[j]).width + letterSpacing;
+            }
+        });
+        
+        y = currentY + parseInt(font, 10) * lineHeight;
+    });
+
+    return currentY;
+}
+
+function downloadImage() {
+    const canvas = document.getElementById('canvas');
+    const link = document.createElement('a');
+    link.download = 'house_info.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+}
